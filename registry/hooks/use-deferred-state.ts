d961dd946865f974357d392740997a2e6e7fe24a
Valerii Strilets <valerii.strilets@gmail.com>
2025-04-15 13:34:51 +0300

    minor

--- registry/hooks/use-deferred-state.ts
+++ registry/hooks/use-deferred-state.ts
@@ -4,9 +4,9 @@ import * as React from 'react'
 
 export function useDeferredState<S>(initialState: S | (() => S)): [S, React.Dispatch<React.SetStateAction<S>>]
 export function useDeferredState<S = undefined>(): [S | undefined, React.Dispatch<React.SetStateAction<S | undefined>>]
-export function useDeferredState<S = undefined>(initialState?: S | (() => S)) {
+export function useDeferredState<S>(initialState?: S | (() => S)) {
   const [value, setValue] = React.useState<S>(initialState as S)
   const deferredValue = React.useDeferredValue(value)
 
-  return [deferredValue, setValue] as const
+  return [deferredValue, setValue]
 }
